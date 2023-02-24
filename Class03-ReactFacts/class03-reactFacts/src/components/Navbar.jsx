e1f1e5214cb9f4649998f224280b79789ed0244a
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-02-23 16:55:17 -0800

    Create a new project like Window Tracker
    This project read what I am doing like what width my browser is

--- Class03-ReactFacts/class03-reactFacts/src/components/Navbar.jsx
+++ Class03-ReactFacts/class03-reactFacts/src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import React from "react"
 
 export default function Navbar() {
+    const [changeColor, setChangeColor] = React.useState(true)
+    
+    function setProp(prop, value) {
+        document.documentElement.style.setProperty(prop, value)
+    }
+
+    function checkColor() {
+        setChangeColor(prevChangeColor => !prevChangeColor)
+        setColor()
+    }
+
+    function setColor(){
+        window.console.log(changeColor)
+
+        if (changeColor === true) {
+            setProp('--background', "#282D35");
+            setProp('--backgroundNav', "#21222A"); 
+            setProp('--text', "#ffffff");
+        } 
+        else if (changeColor === false) {
+            setProp('--background', "#fff5ff");
+            setProp('--backgroundNav', "#ffffff");
+            setProp('--text', "#222222");
+        }
+    }
+
     return (
         <nav>
             <img src="./src/images/react-icon-small.png" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <h4>Light</h4>
+            <input class="apple--switch" type="checkbox" onClick={checkColor}/>
+            <h4>Dark</h4>
         </nav>
     )
 }
