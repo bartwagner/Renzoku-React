2a4c7d975409ca235845059cca6e734e901ff0fa
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-02-27 11:32:01 -0800

    Example centralize Objects and OnMouseHouver

--- Class03-ReactFacts/class03-reactFacts/src/components/Navbar.jsx
+++ Class03-ReactFacts/class03-reactFacts/src/components/Navbar.jsx
@@ -1,39 +1,17 @@
 import React from "react"
 
-export default function Navbar() {
-    const [changeColor, setChangeColor] = React.useState(true)
-    
-    function setProp(prop, value) {
-        document.documentElement.style.setProperty(prop, value)
-    }
-
-    function checkColor() {
-        setChangeColor(prevChangeColor => !prevChangeColor)
-        setColor()
-    }
-
-    function setColor(){
-        window.console.log(changeColor)
-
-        if (changeColor === true) {
-            setProp('--background', "#282D35");
-            setProp('--backgroundNav', "#21222A"); 
-            setProp('--text', "#ffffff");
-        } 
-        else if (changeColor === false) {
-            setProp('--background', "#fff5ff");
-            setProp('--backgroundNav', "#ffffff");
-            setProp('--text', "#222222");
-        }
-    }
-
+export default function Navbar(props) {
     return (
-        <nav>
+        <nav className={props.darkMode ? "dark": ""}>
             <img src="./src/images/react-icon-small.png" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4>Light</h4>
-            <input class="apple--switch" type="checkbox" onClick={checkColor}/>
-            <h4>Dark</h4>
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
