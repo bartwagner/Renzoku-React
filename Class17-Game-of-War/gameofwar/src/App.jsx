d7bac5032479e08efdde9f6a0f7371dff8b5df12
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-04-04 14:27:18 -0700

    I was learning about callback function

--- Class17-Game-of-War/gameofwar/src/App.jsx
+++ Class17-Game-of-War/gameofwar/src/App.jsx
@@ -14,17 +14,60 @@ function App() {
     };
 
     await fetch ("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", options)
-    .then (res => res.json())
+    .then (response => response.json())
     .then (data => window.console.log(data))
     .catch([])
   }
-
-  function callback(){
-    window.console.log("I finally ran!")
-  }
-
-  setTimeout(callback, 2000)
   
+
+
+
+
+
+
+  
+
+
+  // const people = [
+  //   { name: "Jack", hasPet: true },
+  //   { name: "Jill", hasPet: false },
+  //   { name: "Alice", hasPet: true },
+  //   { name: "Bob", hasPet: false },
+  // ]
+
+  // function filterArray(array, callback) {
+  //   const resultingArray = []
+  //   // Write your filtering logic here
+  //   for(let item of array){
+  //     const shouldBeIncluded = callback(item)
+  //     if(shouldBeIncluded){
+  //       resultingArray.push(item)
+  //     }
+  //   }
+  //   return resultingArray
+  // }
+
+  // const peopleWithPets = filterArray(people, function(person) {
+  //   return person.hasPet
+  // })
+
+  // console.log(peopleWithPets)
+
+
+  // function callback(){
+  //   window.console.log("I finally ran!")
+  // }
+
+  // setTimeout(callback, 2000)
+
+
+  // function gimmeThePets(person) {
+  //   return person.hasPet
+  // }
+  // //const peopleWithPets = people.filter(gimmeThePets)
+
+
+  // window.console.log(peopleWithPets)
 
   return (
     <div className="App">
--- Class17-Game-of-War/gameofwar/src/Components/Body.jsx
+++ Class17-Game-of-War/gameofwar/src/Components/Body.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Body(props) {
   return (
     <div className="Body">
-        <button onClick={props.requestDesk}>New Deck, Please!</button>
+        <button id="new-deck" onClick={props.requestDesk}>New Deck, Please!</button>
     </div>
   )
 }
