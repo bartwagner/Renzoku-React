cfbe0306c3e12b1a3967be9dc2405e61d53c4813
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-05-01 13:46:32 -0700

    handling the errors

--- Class19-Dashboard/dashboard/src/App.jsx
+++ Class19-Dashboard/dashboard/src/App.jsx
@@ -5,33 +5,26 @@ import Body from './Components/Body'
 function App() {
   
   const [dataInforImage, setDataInforImage] = React.useState()
-
   const [dataCurrencyCad, setDataCurrencyCad] = React.useState()
+  let imgInfor = {
+    id: "",
+    nameAuthor: "",
+    url: ""
+  }
 
   React.useEffect(()=> {
     async function resquestApiBackground() {
+      
       // Image and Author
-
-      const imgInfor = {
-        id: "",
-        nameAuthor: "",
-        imgCrypto:"",
-        url: ""
-      }
-
       try{
         const responseImage = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
         if(!responseImage.ok){
           throw Error("API has a problem")
         }
         const dataImage     = await responseImage.json()
-
-        window.console.log(dataImage)
         if(dataImage.errors)
         {
-          imgInfor.id = "jlVEj8IDPQc",
-          imgInfor.nameAuthor = "Simon Wilkes",
-          imgInfor.url = "https://images.unsplash.com/photo-1528184039930-bd03972bd974?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI1NDkzODA&ixlib=rb-4.0.3&q=85"
+          erroImgAuthor()
         }else{
           imgInfor.id = dataImage.id,
           imgInfor.nameAuthor = dataImage.user.name,
@@ -39,28 +32,54 @@ function App() {
         }
       }
       catch(err){
-        imgInfor.id = "jlVEj8IDPQc",
-        imgInfor.nameAuthor = "Simon Wilkes",
-        imgInfor.url = "https://images.unsplash.com/photo-1528184039930-bd03972bd974?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI1NDkzODA&ixlib=rb-4.0.3&q=85"
+        erroImgAuthor()
       }
-
       setDataInforImage(imgInfor)
 
       // Crypto Currency
       const responseCurrency = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin?localization=false")
+      const CurrencyInfor = {
+        price: "",
+        highPrice: "",
+        lowPrice: "",
+        nameCurrency: "",
+        imgCurrency: ""
+      }
+      
       if(!responseCurrency.ok){
-        setDataCurrencyCad("Unavailable this moment")
+        CurrencyInfor.price = "Unavailable this moment"
+        CurrencyInfor.imgCurrency = 'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579'
+        setDataCurrencyCad(CurrencyInfor)
       }else{
         const dataCurrency     = await responseCurrency.json()
+
+        window.console.log(dataCurrency)
+
         let num = dataCurrency.market_data.current_price.cad
         var formatNum = " $" + num.toLocaleString('en-US') + " CAD"
-        setDataCurrencyCad(formatNum)
+        let numHigh = dataCurrency.market_data.high_24h.cad
+        var formatNumHigh = " $" + numHigh.toLocaleString('en-US') + " CAD"
+        let numLow = dataCurrency.market_data.low_24h.cad
+        var formatNumLow = " $" + numLow.toLocaleString('en-US') + " CAD"
+
+        CurrencyInfor.price = formatNum
+        CurrencyInfor.highPrice = formatNumHigh
+        CurrencyInfor.lowPrice = formatNumLow
+        CurrencyInfor.imgCurrency = dataCurrency.image.thumb
+        CurrencyInfor.nameCurrency = dataCurrency.name
+        setDataCurrencyCad(CurrencyInfor)
       }
     }
     resquestApiBackground()
   }, [])
 
-  if (!dataInforImage){
+  function erroImgAuthor(){
+    imgInfor.id = "jlVEj8IDPQc",
+    imgInfor.nameAuthor = "Simon Wilkes",
+    imgInfor.url = "https://images.unsplash.com/photo-1528184039930-bd03972bd974?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI1NDkzODA&ixlib=rb-4.0.3&q=85"
+  }
+
+  if (!(dataCurrencyCad && dataInforImage)){
     return (
       <div className="c--loader"/>
     )
@@ -71,9 +90,13 @@ function App() {
     <div>
       <Body
         key={dataInforImage.id}
-        author={dataInforImage.nameAuthor}
-        cryptoBitcoin={dataCurrencyCad}
-          />
+        author       = {dataInforImage.nameAuthor}
+        cryptoPrice  = {dataCurrencyCad.price}
+        cryptoHigh   = {dataCurrencyCad.highPrice}
+        cryptoLow    = {dataCurrencyCad.lowPrice}
+        cryptoImg    = {dataCurrencyCad.imgCurrency}
+        nameCurrency = {dataCurrencyCad.nameCurrency}
+      />
     </div>
   )
 }
--- Class19-Dashboard/dashboard/src/Components/Body.jsx
+++ Class19-Dashboard/dashboard/src/Components/Body.jsx
@@ -6,15 +6,19 @@ return(
     <main className='main'>
         <div className="top--website">
             <div className='crypto--Website'>
-                <p>Crypto: </p>
-                <img className='crypto--simbol' src={} />
-                <p className='crypto--value'>  {props.cryptoBitcoin}</p>
+                <div className='crypto--name'>
+                    <img className='crypto--simbol' src={props.cryptoImg} />
+                    <p className='crypto--value'>{props.nameCurrency}</p>
+                </div>
+                <div>
+                    <p className='crypto--value'>🎯: {props.cryptoPrice}</p>
+                    <p className='crypto--value'>👆: {props.cryptoHigh}</p>
+                    <p className='crypto--value'>👇: {props.cryptoLow}</p>
+                </div>
             </div>
             <p>weather</p>
         </div>
-
         <h1 className='time--website'>TIME HERE</h1>
-
         <p>By: {props.author}</p> 
     </main>
 )
